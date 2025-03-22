4141a34b2c920a110d68a1153c4a4acab182a678
Mohith-113 <vaidyamanch@gmail.com>
2025-03-22 12:29:06 +0530

    initial comment for setup

---
+++ travel/src/main/java/travel/route/Navigation.java
@@ -0,0 +1,19 @@
+package travel.route;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/")
+public class Navigation extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/template/index.jsp");
+		dispatcher.forward(request, response);
+	}
+	
+}
